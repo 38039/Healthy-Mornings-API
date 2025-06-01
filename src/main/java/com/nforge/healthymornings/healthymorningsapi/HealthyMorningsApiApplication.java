b9b3b94d03c4d817ad9964b49eeabe42283aa731
Miłosz <38039@student.atar.edu.pl>
2025-06-01 16:34:17 +0200

    Initial commit

---
+++ src/main/java/com/nforge/healthymornings/healthymorningsapi/HealthyMorningsApiApplication.java
@@ -0,0 +1,13 @@
+package com.nforge.healthymornings.healthymorningsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyMorningsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyMorningsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nforge/healthymornings/healthymorningsapi/HealthyMorningsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nforge.healthymornings.healthymorningsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyMorningsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
