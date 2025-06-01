a13f14fce1f4dd8f4576a7ea34665e031a5d9bc8
Miłosz <38039@student.atar.edu.pl>
2025-06-01 21:02:22 +0200

    - Zaimplementowano model encji Level tabeli levels

---
+++ src/main/java/com/nforge/healthymorningsapi/model/Level.java
@@ -0,0 +1,41 @@
+package com.nforge.healthymorningsapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+
+@Entity @NoArgsConstructor @AllArgsConstructor @Builder
+@Table(name = "levels", schema = "application")
+@Getter @Setter
+public class Level {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Column(name = "id_level")
+    private Integer id;
+
+    @Column(name = "name", unique = true)
+    @NotBlank
+    private String name;
+
+    @Getter @Column(name = "description")
+    private String description;
+
+    @Getter @Column(name = "level_up_message")
+    private String levelUpMessage;
+
+    @Getter @Column(name = "minimum_points")
+    @NotNull @Min(0) // Wartość musi być pozytywna
+    private Long minimumPoints;
+
+    @Getter @Column(name = "maximum_points")
+    @NotNull @Min(0)
+    private Long maximumPoints;
+
+    @Getter @Column(name = "reward_bonus")
+    @Min(0)
+    private Long rewardBonus;
+
+}
