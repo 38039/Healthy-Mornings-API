eecb62caafe258584fa99c9fcd530c8081965dbd
Miłosz <38039@student.atar.edu.pl>
2025-06-01 19:34:32 +0200

    q

--- src/main/java/com/nforge/healthymorningsapi/HealthyMorningsApiApplication.java
+++ src/main/java/com/nforge/healthymorningsapi/HealthyMorningsApiApplication.java
@@ -1,4 +1,4 @@
-package com.nforge.healthymornings.healthymorningsapi;
+package com.nforge.healthymorningsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/nforge/healthymorningsapi/HealthyMorningsApiApplicationTests.java
+++ src/test/java/com/nforge/healthymorningsapi/HealthyMorningsApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nforge.healthymornings.healthymorningsapi;
+package com.nforge.healthymorningsapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
