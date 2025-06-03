a84082532f66145ae2104acfcb92a840e9f84a0a
Miłosz <38039@student.atar.edu.pl>
2025-06-03 17:11:51 +0200

    Zaimplementowano model danych przesyłanych przez klienta wraz z żądaniem logowania

---
+++ src/main/java/com/nforge/healthymorningsapi/payload/LoginRequest.java
@@ -0,0 +1,21 @@
+// Model danych przesyłanych do API z zapytaniem w formacie JSON,
+// który przechowuje podane przez klienta dane do logowania
+package com.nforge.healthymorningsapi.payload;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter @Setter
+public class LoginRequest {
+
+    @NotBlank @Email(message = "Podany email powinien trzymać się struktury email@domain.country")
+    private String email;
+
+    @NotBlank(message = "Podane hasło nie może być puste")
+    @Size(min = 6, max = 100, message = "Hasło musi mieć minimum 6 znaków")
+    private String password;
+}
