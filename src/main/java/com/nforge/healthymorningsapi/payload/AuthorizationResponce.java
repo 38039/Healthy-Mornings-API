9197b49d96bc4c97120f30e6d249b9d9345edf5d
Miłosz <38039@student.atar.edu.pl>
2025-06-04 23:13:38 +0200

    Fix: Naprawiono bug, w wyniku którego logowanie nie zwracało danych (tokenu i jego ważności)

--- src/main/java/com/nforge/healthymorningsapi/payload/AuthorizationResponce.java
+++ src/main/java/com/nforge/healthymorningsapi/payload/AuthorizationResponce.java
@@ -2,8 +2,12 @@ package com.nforge.healthymorningsapi.payload;
 
 
 public class AuthorizationResponce {
-    private String token;
-    private long expiresIn;
+    private String  token;
+    public  String  getToken()      { return token; }
+
+    private long    expiresIn;
+    public  long    getExpiresIn()  { return expiresIn; }
+
 
     public AuthorizationResponce setToken(String token) {
         this.token = token;
@@ -13,5 +17,14 @@ public class AuthorizationResponce {
     public AuthorizationResponce setExpiresIn(long expiresIn) {
         this.expiresIn = expiresIn;
         return this;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "LoginResponse{"
+                        + "token='"       + token     + '\''
+                        + ", expiresIn="  + expiresIn +
+                '}';
     }
 }
